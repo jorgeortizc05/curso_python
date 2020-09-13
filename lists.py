c09270178f8c143de5235dd77493a2ab1e01e9b0
jorgeortizc05 <47234672+jorgeortizc05@users.noreply.github.com>
2020-09-13 15:56:45 -0500

    firts commit

---
+++ lists.py
@@ -0,0 +1,13 @@
+#
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+print (names)
+
+print(names[0]) #Obtengo el primer elemento
+
+names.append("Draco") #Aniade un elemento
+
+print(names)
+
+names.sort() #Le invierte a la cadena, el ultimo es primero etc
+
+print(names)
